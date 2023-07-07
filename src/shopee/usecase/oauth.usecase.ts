e63fcfb997230810bef93b615c54e43fa2a123e4
Aom Siriwat <siriwat576@gmail.com>
2023-07-08 00:33:48 +0700

    feat: Add function to get Shopee client credentials
    This commit adds a new function `getClient()` to retrieve the Shopee client credentials (shopId, partnerId, partnerKey) from the environment variables and validate them. If the credentials are valid, it returns a new instance of the ShopeeClient. If the credentials are missing or invalid, it returns null.
    
    The `OAuthUsecase` class constructor is modified to use the `getClient()` function to initialize the `client` property, instead of directly setting the values from the environment variables.
    
    This change ensures that the client credentials are properly validated before creating an instance of the ShopeeClient, and improves the code structure by isolating the credentials retrieval logic into a separate function.

--- src/shopee/usecase/oauth.usecase.ts
+++ src/shopee/usecase/oauth.usecase.ts
@@ -1,21 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import ShopeeClient from "shopee-client";
 
+function getClient() {
+    const shopId = process.env.SHOPEE_SHOP_ID;
+    const partnerId = process.env.SHOPEE_PARTNER_ID;
+    const partnerKey = process.env.SHOPEE_PARTNER_KEY;
+
+    if(!shopId || !partnerId || !partnerKey) return null;
+    if(typeof shopId !== "string" || typeof partnerId !== "string" || typeof partnerKey !== "string") return null;
+    if(shopId.length <= 0 || partnerId.length <= 0 || partnerKey.length <= 0) return null;
+    return new ShopeeClient({
+        is_uat: false,
+        shop_id: shopId,
+        partner_id: partnerId,
+        partner_key: partnerKey,
+    });
+}
 @Injectable()
 export class OAuthUsecase {
     private readonly shopId: string;
     private readonly client: ShopeeClient;
     constructor() {
         this.shopId = process.env.SHOPEE_SHOP_ID;
-        const partnerId = process.env.SHOPEE_PARTNER_ID;
-        const partnerKey = process.env.SHOPEE_PARTNER_KEY;
-        const validEnv = partnerId && partnerKey && partnerId.length > 0 && partnerKey.length > 0;
-        this.client = validEnv ? new ShopeeClient({
-            is_uat: false,
-            shop_id: this.shopId,
-            partner_id: partnerId,
-            partner_key: partnerKey,
-        }) : null;
+        this.client = getClient();
     }
 
     buildRedirectUrl() {
